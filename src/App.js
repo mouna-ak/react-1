f9ede79d716da754f7b45dccce26c45f2dd4cdf4
mouna-ak <monaakebli@gmail.com>
2023-03-23 14:48:56 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import BasicExample from "./components/Form";
+import Mynavbar from "./components/Layout/Mynavbar";
+function App() {
+  return (
+    <>
+      <Mynavbar />
+      <BasicExample />
+    </>
+  );
+}
+
+export default App;
